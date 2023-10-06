77aebc7ec34fcdb6688e216988ce5af902e8fd2a
wuqlan111 <wuqlan111@163.com>
2023-10-06 11:21:19 +0800

    Modify gpio.

--- u-boot-orangepi_3lts/drivers/gpio/allwinner_h6_gpio.c
+++ u-boot-orangepi_3lts/drivers/gpio/allwinner_h6_gpio.c
@@ -6,11 +6,13 @@
 #include <clk.h>
 #include <dm.h>
 #include <dm/device_compat.h>
+#include <dm/device.h>
 #include <malloc.h>
 #include <asm/io.h>
 #include <linux/sizes.h>
 #include <linux/io.h>
 #include <asm/gpio.h>
+#include <asm/arch/gpio.h>
 
 #include  "allwinner_h6_gpio.h"
 
@@ -38,16 +40,19 @@ typedef struct {
 } allwinner_h6_gpio_plat_t;
 
 
-static int32_t allwinner_h6_gpio_direction_input(struct udevice *dev, uint32_t pin)
+static allwinner_h6_gpio_config_t * allwinner_pio_get_port_cfg(const uint32_t port)
 {
-	allwinner_h6_gpio_plat_t * plat = dev_get_plat(dev);
-	allwinner_h6_gpio_config_t *  config_regs  =  
-									(allwinner_h6_gpio_config_t * )plat->config_base;
-
-	if (pin >= plat->gpio_count) {
-		dev_err(dev, "pin_id [%u] invalid", pin);
-		return -EINVAL;
+	if (port > GPIO_MAX_BANK) {
+		return  NULL;
 	}
+	
+	return  (allwinner_h6_gpio_config_t * )GPIO_BANK_ADDR(port);	
+}
+
+
+static int32_t _allwinner_h6_gpio_direction_input(allwinner_h6_gpio_config_t * const config_regs,  
+														const uint32_t pin)
+{
 
 	uint32_t  offset  =  pin  >> 3;
 	uint32_t  shift   =  (pin % 8) << 2;
@@ -59,18 +64,9 @@ static int32_t allwinner_h6_gpio_direction_input(struct udevice *dev, uint32_t p
 }
 
 
-static int32_t allwinner_h6_gpio_direction_output(struct udevice *dev, uint32_t pin,
-				    int32_t val)
+static int32_t _allwinner_h6_gpio_direction_output(allwinner_h6_gpio_config_t * const config_regs, const uint32_t pin,
+				const int32_t val)
 {
-	allwinner_h6_gpio_plat_t * plat = dev_get_plat(dev);
-	allwinner_h6_gpio_config_t *  config_regs  =  
-									(allwinner_h6_gpio_config_t * )plat->config_base;
-
-	if (pin >= plat->gpio_count) {
-		dev_err(dev, "pin_id [%u] invalid", pin);
-		return -EINVAL;
-	}
-
 	uint32_t  offset  =  pin  >> 3;
 	uint32_t  shift   =  (pin % 8) << 2;
 
@@ -86,6 +82,76 @@ static int32_t allwinner_h6_gpio_direction_output(struct udevice *dev, uint32_t 
 }
 
 
+static int32_t _allwinner_h6_gpio_get_value(allwinner_h6_gpio_config_t * const config_regs, const uint32_t pin)
+{
+	return  readl(&config_regs->dat) & (1 << pin)? 1: 0;
+}
+
+static int32_t _allwinner_h6_gpio_set_value(allwinner_h6_gpio_config_t * const config_regs, 
+								const uint32_t pin, const int32_t val)
+{
+	if (val) {
+		setbits_le32(&config_regs->dat,  1 << pin);		
+	} else {
+		clrbits_le32(&config_regs->dat,  1 << pin);
+	}
+
+	return 0;
+}
+
+static int32_t _allwinner_h6_gpio_get_function(allwinner_h6_gpio_config_t * const config_regs, const uint32_t  pin)
+{
+	uint32_t  offset  =  pin  >> 3;
+	uint32_t  shift   =  (pin % 8) << 2;
+	uint32_t  pin_func  =  (readl(&config_regs->cfgx[offset]) >> shift) & 0x7;
+
+	if (pin_func ==  ALLWINNER_H6_GPIO_OUTPUT) {
+		return  GPIOF_OUTPUT;
+	} else if (pin_func ==  ALLWINNER_H6_GPIO_INPUT ) {
+		return  GPIOF_INPUT;
+	} else if (pin_func == ALLWINNER_H6_GPIO_OUTPUT) {
+		return  GPIOF_UNUSED;
+	}
+
+	return GPIOF_FUNC;
+}
+
+
+
+#if CONFIG_IS_ENABLED(DM_GPIO)
+
+
+static int32_t allwinner_h6_gpio_direction_input(struct udevice *dev, uint32_t pin)
+{
+	allwinner_h6_gpio_plat_t * plat = dev_get_plat(dev);
+	allwinner_h6_gpio_config_t *  config_regs  =  
+									(allwinner_h6_gpio_config_t * )plat->config_base;
+
+	if (pin >= plat->gpio_count) {
+		dev_err(dev, "pin_id [%u] invalid", pin);
+		return -EINVAL;
+	}
+
+	return  _allwinner_h6_gpio_direction_input(config_regs,  pin);
+}
+
+
+static int32_t allwinner_h6_gpio_direction_output(struct udevice *dev, uint32_t pin,
+				    int32_t val)
+{
+	allwinner_h6_gpio_plat_t * plat = dev_get_plat(dev);
+	allwinner_h6_gpio_config_t *  config_regs  =  
+									(allwinner_h6_gpio_config_t * )plat->config_base;
+	
+	if (pin >= plat->gpio_count) {
+		dev_err(dev, "pin_id [%u] invalid", pin);
+		return -EINVAL;
+	}
+
+	return  _allwinner_h6_gpio_direction_output(config_regs,  pin,  val);
+}
+
+
 static int32_t allwinner_h6_gpio_get_value(struct udevice *dev, uint32_t pin)
 {
 	allwinner_h6_gpio_plat_t * plat = dev_get_plat(dev);
@@ -97,7 +163,7 @@ static int32_t allwinner_h6_gpio_get_value(struct udevice *dev, uint32_t pin)
 		return -EINVAL;
 	}
 
-	return  readl(&config_regs->dat) & (1 << pin)? 1: 0;
+	return  _allwinner_h6_gpio_get_value(config_regs,  pin);
 }
 
 
@@ -112,13 +178,7 @@ static int32_t allwinner_h6_gpio_set_value(struct udevice *dev, uint32_t pin, in
 		return -EINVAL;
 	}
 
-	if (val) {
-		setbits_le32(&config_regs->dat,  1 << pin);		
-	} else {
-		clrbits_le32(&config_regs->dat,  1 << pin);
-	}
-
-	return 0;
+	return  _allwinner_h6_gpio_set_value(config_regs,  pin, val);
 }
 
 
@@ -133,19 +193,7 @@ static int32_t allwinner_h6_gpio_get_function(struct udevice *dev, uint32_t  pin
 		return -EINVAL;
 	}
 
-	uint32_t  offset  =  pin  >> 3;
-	uint32_t  shift   =  (pin % 8) << 2;
-	uint32_t  pin_func  =  (readl(&config_regs->cfgx[offset]) >> shift) & 0x7;
-
-	if (pin_func ==  ALLWINNER_H6_GPIO_OUTPUT) {
-		return  GPIOF_OUTPUT;
-	} else if (pin_func ==  ALLWINNER_H6_GPIO_INPUT ) {
-		return  GPIOF_INPUT;
-	} else if (pin_func == ALLWINNER_H6_GPIO_OUTPUT) {
-		return  GPIOF_UNUSED;
-	}
-
-	return GPIOF_FUNC;
+	return  _allwinner_h6_gpio_get_function(config_regs,  pin);
 }
 
 
@@ -195,6 +243,78 @@ U_BOOT_DRIVER(allwinner_h6_gpio) = {
 	.plat_auto	= sizeof(allwinner_h6_gpio_plat_t),
 	.probe		= allwinner_h6_gpio_probe,
 };
+
+#else
+
+#define  GPIO_TO_PORT(gpio)   ((gpio) >> 5)
+#define  GPIO_TO_PIN(gpio)    ((gpio) & 0x1f)
+
+int32_t gpio_request(uint32_t gpio, const char *label)
+{
+	return 0;
+}
+
+int32_t gpio_free(uint32_t gpio)
+{
+	return 0;
+}
+
+int32_t gpio_direction_input(uint32_t gpio)
+{
+	uint32_t  port = GPIO_TO_PORT(gpio);
+	uint32_t  pin  =  GPIO_TO_PIN(gpio);
+
+	allwinner_h6_gpio_config_t *  config_regs  =  allwinner_pio_get_port_cfg(port);
+	if (config_regs == NULL) {
+		return  -1;
+	}
+
+	return  _allwinner_h6_gpio_direction_input(config_regs,  pin);
+}
+
+int32_t gpio_direction_output(uint32_t gpio, int32_t value)
+{
+	uint32_t  port = GPIO_TO_PORT(gpio);
+	uint32_t  pin  =  GPIO_TO_PIN(gpio);
+
+	allwinner_h6_gpio_config_t *  config_regs  =  allwinner_pio_get_port_cfg(port);
+	if (config_regs == NULL) {
+		return  -1;
+	}
+
+	return  _allwinner_h6_gpio_direction_output(config_regs,  pin,  0);
+
+}
+
+int32_t gpio_get_value(uint32_t gpio)
+{
+	uint32_t  port = GPIO_TO_PORT(gpio);
+	uint32_t  pin  =  GPIO_TO_PIN(gpio);
+
+	allwinner_h6_gpio_config_t *  config_regs  =  allwinner_pio_get_port_cfg(port);
+	if (config_regs == NULL) {
+		return  -1;
+	}
+
+	return  _allwinner_h6_gpio_get_value(config_regs,  pin);
+
+}
+
+int32_t gpio_set_value(uint32_t gpio, int32_t value)
+{
+	uint32_t  port = GPIO_TO_PORT(gpio);
+	uint32_t  pin  =  GPIO_TO_PIN(gpio);
+
+	allwinner_h6_gpio_config_t *  config_regs  =  allwinner_pio_get_port_cfg(port);
+	if (config_regs == NULL) {
+		return  -1;
+	}
+
+	return  _allwinner_h6_gpio_set_value(config_regs,  pin,  value);
+
+}
+
+#endif
 
 
 
