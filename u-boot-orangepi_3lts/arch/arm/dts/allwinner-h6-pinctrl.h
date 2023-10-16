ed4c67aea0ce574a7fd95911126de47a67a8ac9f
wuqlan111 <wuqlan111@163.com>
2023-10-17 01:05:46 +0800

    Modify    uart0 pinctrl.

--- u-boot-orangepi_3lts/arch/arm/dts/allwinner-h6-pinctrl.h
+++ u-boot-orangepi_3lts/arch/arm/dts/allwinner-h6-pinctrl.h
@@ -3,7 +3,7 @@
 #define __DTS_ALLWINNER_H6_PINFUNC_H
 
 
-/**/
+/*pinctrl config  <pin func>*/
 #define  ALLWINNER_H6_PC0_NAND_WE           0    2
 #define  ALLWINNER_H6_PC0_SPI0_CLK          0    4
 #define  ALLWINNER_H6_PC1_NAND_ALE          1    2
@@ -14,8 +14,8 @@
 
 
 
-
-
+#define  ALLWINNER_H6_PF2_UART0_TX          2    3
+#define  ALLWINNER_H6_PF4_UART0_RX          4    3
 
 
 
